3295473ed5650063b371e7d6845044494920125f
Daniel <damesaa@eafit.edu.co>
2021-10-22 20:00:03 -0500

    add

--- src/main/java/co/edu/eafit/protobootapp/Calculadora.java
+++ src/main/java/co/edu/eafit/protobootapp/Calculadora.java
@@ -5,7 +5,7 @@ public class Calculadora {
     public Double cuadrado(Double numero) {
         return numero * numero;
     }
-
+    
     public Double cubo(Double numero) {
         return numero * numero * numero;
     }
